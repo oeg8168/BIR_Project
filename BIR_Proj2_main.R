6b202dff2d8451b9136a2409a6b2906e8d9d2169
oeg8168 <oeg8168@gmail.com>
2014-10-19 17:51:00 +0800

    Revert "- Source all function"
    This reverts commit 86a518698d4a12f7f1dbf8c4db278d6a0044f4b8.

--- BIR_Proj2_main.R
+++ BIR_Proj2_main.R
@@ -6,29 +6,11 @@
 rm(list=ls())   # clear all data
 cat("\014")     # clear all console
 
-# Source all files under "./function" folder
-functions <- list.files(path = "./function/",
-                        full.names = T)
-sapply(functions, FUN = source)
-
-# Source all files under "./class" folder
-classes <- list.files(path = "./class/",
-                      full.names = T)
-sapply(functions, FUN = source)
-
-inputFolder <- "./input/#2"
+inputFolder <- "./input/#2/"
 filesPath <- list.files(path = inputFolder, 
                         pattern = "*.xml", 
                         recursive = T, 
                         full.names = T)
-
-
-getXmlContent(filesPath[1], "AbstractText")
-getXmlContent(filesPath[11], "AbstractText")
-getXmlContent(filesPath[113], "AbstractText")
-
-lapply(filesPath[1:113],FUN = getXmlContent,"AbstractText")
-
 
 
 
