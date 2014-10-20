48c146557a003c0a2e1d0bc033f17f53139e77a7
oeg8168 <oeg8168@gmail.com>
2014-10-20 20:31:28 +0800

    Try parsing XML files into class "Document"

--- BIR_Proj2_main.R
+++ BIR_Proj2_main.R
@@ -14,7 +14,7 @@ sapply(functions, FUN = source)
 # Source all files under "./class" folder
 classes <- list.files(path = "./class/",
                       full.names = T)
-sapply(functions, FUN = source)
+sapply(classes, FUN = source)
 
 # Get list of input XML files
 inputFolder <- "./input/#2"
@@ -23,16 +23,29 @@ filesPath <- list.files(path = inputFolder,
                         recursive = T, 
                         full.names = T)
 
+# Vector for storing raw contents of XML files
+rawContents <- character(0)
+
 # Vector for storing index of encoding error
 readXmlErrIndex <- numeric(0)
 
 # Read XML files and deal with encoding error
-for(i in 1:1477){
+for(i in 1:length(filesPath)){
+    #tempDoc <- new("Document", path = filesPath[i])
     tryCatch(
-        getXmlContent(filesPath[i], "AbstractText"),
+        rawContents <- c(rawContents, getXmlContent(filesPath[i], "AbstractText")),
         error = function(e){
             cat(paste0("Index: ", i, "\n"))
             readXmlErrIndex <<- c(readXmlErrIndex, i)
         }
     )
 }
+
+# have problem
+v <- new("Document")
+lapply(1:length(rawContents), function(i){
+        tempDoc <- new("Document", content = rawContents[i])
+        tempDoc <- parseXmlData(tempDoc)
+        v <<- c(v, tempDoc)
+    }
+)
