5a4780e47f7bca891e970db6916b8a0898e1698c
oeg8168 <oeg8168@gmail.com>
2014-11-06 20:58:02 +0800

    Packaging previous works

---
+++ function/parseXmlDocSet.R
@@ -0,0 +1,62 @@
+####################
+# 
+#   Parse XML document collection and process words
+#   (Packaging previous works)
+#
+
+parseXmlDocSet <- function(inputXmlDocSet,
+                           stopWordFile = "./input/stopwords.txt",
+                           removeStopWord = TRUE,
+                           removeNumber = TRUE,
+                           stemming = TRUE,
+                           wordDocRelation = FALSE,
+                           reportAll = FALSE)
+{
+    require(XML)
+    require(Rstem)
+        
+    # Parse XML file
+    xmlFile <- xmlParse(inputXmlDocSet, encoding = "UTF-8")
+    
+    # Get PubMed ID of article
+    PMID <- xpathSApply(xmlFile, "//*/MedlineCitation/PMID", xmlValue)
+    
+    # Get abstract content of article
+    content <- xpathSApply(xmlFile, "//*/Abstract", xmlValue)
+    
+    # Split content into words
+    word <- strsplit(tolower(content), "[[:space:]]+|[[:punct:]]+")
+    
+    # Remove empty words
+    word <- lapply(word, function(input) input[input!=""])
+    
+    # Get stop words
+    stopWords <- scan(stopWordFile, what = character())
+    
+    # Start process words
+    wordProcessed <- word
+    
+    # (optional) Remove stop words
+    if(removeStopWord)
+        wordProcessed <- lapply(wordProcessed, 
+                                function(input) input[!input%in%stopWords])
+    
+    # (optional) Remove number
+    if(removeNumber)
+        wordProcessed <- lapply(wordProcessed, 
+                                function(input) input[!grepl("^[[:digit:]]+$", input)])
+    
+    # (optional) Word stemming
+    if(stemming)
+        wordProcessed <- lapply(wordProcessed, 
+                                wordStem)
+    
+    # Combine all informations
+    raw <- cbind(PMID, content, word, wordProcessed)
+    
+    # Convert result as data frame
+    raw <- as.data.frame(raw, stringsAsFactors = FALSE)
+        
+    return(raw)
+}
+    
