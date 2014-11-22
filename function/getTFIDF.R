14ac5991ab6434f9f723dee8d84f4fe5f4c3ba98
oeg8168 <oeg8168@gmail.com>
2014-11-22 16:25:06 +0800

    Little modified and add comments

--- function/getTFIDF.R
+++ function/getTFIDF.R
@@ -5,26 +5,26 @@
 
 getTFIDF <- function(doc, wordFreq, processed = T){
     
+    # Determine what word will be use (origin / processed)
     if(processed)
         docWord <- doc$wordProcessed
     else
         docWord <- doc$word
     
+    # Collect words and construct freq table
     result <- data.frame(table(docWord))
     
+    # Save IDF
     result$IDF <- wordFreq$IDF[sapply(result$docWord, function(term) which(wordFreq$word %in% term))]
-        
-    TF_WIKI <- result$Freq / sum(result$Freq)
     
-    TF_PPT <- 1 + log10(result$Freq)
+    # Count TF-IDF by formula from WIKI
+    result$TFIDF_WIKI <- (result$Freq / sum(result$Freq)) * result$IDF
     
-    TF_OTHER <- result$Freq # need to be modify
+    # Count TF-IDF by formula from course PPT
+    result$TFIDF_PPT <- (1 + log10(result$Freq)) * result$IDF
     
-    result$TFIDF_WIKI <- TF_WIKI * result$IDF
-    
-    result$TFIDF_PPT <- TF_PPT * result$IDF
-    
-    result$TFIDF_OTHER <- TF_OTHER * result$IDF
+    # Count TF-IDF by formula from paper
+    result$TFIDF_OTHER <- (log10(1 + (1 + log10(result$Freq)))) * result$IDF
     
     return(result)    
 }
